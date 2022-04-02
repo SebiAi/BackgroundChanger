14b205e96085b2e8c78f6387858b5ef44499468e
SebiAi <sebastian.ai@gmx.at>
2022-04-02 15:26:35 +0200

    Initial commit

---
+++ app/src/main/java/com/sebiai/backgroundchanger/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sebiai.backgroundchanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    private BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setup();
+    }
+
+    private void setup() {
+        navController = ((NavHostFragment) Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment))).getNavController();
+
+        bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+}
